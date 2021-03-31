452d52df24f5eb29715a66d40996327637fc27be
Your Name <you@example.com>
2021-03-30 23:06:32 -0300

    Create List to quest and show. Close #2.

--- lib/main.dart
+++ lib/main.dart
@@ -25,15 +25,12 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
-  List<Icon> scoreKeeper = [
-    Icon(
-      Icons.check,
-      color: Colors.green,
-    ),
-    Icon(
-      Icons.close,
-      color: Colors.red,
-    ),
+  List<Icon> scoreKeeper = [];
+
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.',
   ];
 
   @override
@@ -48,7 +45,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions.first,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -74,6 +71,7 @@ class _QuizPageState extends State<QuizPage> {
               onPressed: () {
                 //The user picked true.
                 setState(() {
+                  questions.remove(questions.first);
                   scoreKeeper.add(
                     Icon(
                       Icons.check,
@@ -100,6 +98,7 @@ class _QuizPageState extends State<QuizPage> {
               onPressed: () {
                 //The user picked false.
                 setState(() {
+                  questions.remove(questions.first);
                   scoreKeeper.add(
                     Icon(
                       Icons.close,
